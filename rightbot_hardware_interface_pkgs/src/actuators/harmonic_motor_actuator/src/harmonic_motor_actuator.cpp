9bb680d21f006659d5948554ec6b95af1d56b7ca
amrapali-rightbot <amrapali.kh@rightbot.in>
2023-05-09 23:53:42 +0530

    underprogress

--- rightbot_hardware_interface_pkgs/src/actuators/harmonic_motor_actuator/src/harmonic_motor_actuator.cpp
+++ rightbot_hardware_interface_pkgs/src/actuators/harmonic_motor_actuator/src/harmonic_motor_actuator.cpp
@@ -129,6 +129,10 @@ CallbackReturn HarmonicMotorActuator::on_activate(const rclcpp_lifecycle::State 
 	set_profile_acc(default_acceleration_);
 	set_profile_deacc(default_acceleration_);
 
+	set_relative_position( 0);
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	logger_->info("[{}] Setting default acceleration: [{}]",motor_name_, default_acceleration_);
+
     return CallbackReturn::SUCCESS;
 
 }
