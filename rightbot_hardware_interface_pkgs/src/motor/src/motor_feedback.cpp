16cc01fa2d84b599d3516431afa8caaac6f8dc06
dheena-rb23 <dheenadhayalan.r@rightbot.in>
2023-08-29 12:47:44 +0530

    SHER-303 Implement actual current calculation

--- rightbot_hardware_interface_pkgs/src/motor/src/motor_feedback.cpp
+++ rightbot_hardware_interface_pkgs/src/motor/src/motor_feedback.cpp
@@ -66,8 +66,9 @@ int MotorFeedback::motor_status_n_voltage_n_input_states_read(int motor_id, uint
 
         *input_states = (f.data[4] << 0) | (f.data[5] << 8);
 
-        actual_motor_current_register_value = (f.data[6] << 0) | (f.data[7] << 8);
-        *actual_motor_current = 0.01 * static_cast<float>(actual_motor_current_register_value);
+
+        int16_t motor_current_int = ((f.data[4] << 0) | (f.data[5] << 8));
+        *actual_motor_current = ((float)motor_current_int) * 0.01;
     }
 
     return err;
