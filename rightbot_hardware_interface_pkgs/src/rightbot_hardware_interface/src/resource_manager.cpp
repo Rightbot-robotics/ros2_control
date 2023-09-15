6b8dd43200d83a5b3e9eac42f2c61aa48d2a4e6c
amrapali-rightbot <amrapali.kh@rightbot.in>
2023-09-15 12:11:15 +0530

    camera auto align continuosly

--- rightbot_hardware_interface_pkgs/src/rightbot_hardware_interface/src/resource_manager.cpp
+++ rightbot_hardware_interface_pkgs/src/rightbot_hardware_interface/src/resource_manager.cpp
@@ -1118,7 +1118,7 @@ void ResourceManager::read(const rclcpp::Time & time, const rclcpp::Duration & p
     }
      
 
-    if(count > 10) {
+    if(count > 2) {
         count = 1;
         // for (auto & component : resource_storage_->actuators_){ 
 
@@ -1164,7 +1164,7 @@ void ResourceManager::read(const rclcpp::Time & time, const rclcpp::Duration & p
        }
 
        // giving absolute angle to camera
-       if((abs(previous_command_angle - command_angle) > 0.0175) && (abs(command_angle) < 2.35)){
+       if((abs(previous_command_angle - command_angle) > 0.00) && (abs(command_angle) < 2.35)){
                     //
           RCUTILS_LOG_INFO_NAMED(
             "resource_manager", "[camera_align] Command angle '%f', base_rotation_angle '%f'",command_angle, base_rotation_angle);
