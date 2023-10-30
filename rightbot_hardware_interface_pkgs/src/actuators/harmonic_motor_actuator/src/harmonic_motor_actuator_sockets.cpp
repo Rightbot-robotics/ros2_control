1b6c5d5f0868cd49d8e42e305514f337f2bdd6ea
amrapali-rightbot <amrapali.kh@rightbot.in>
2023-10-30 17:35:15 +0530

    Change the can1 and can2 to can0

--- rightbot_hardware_interface_pkgs/src/actuators/harmonic_motor_actuator/src/harmonic_motor_actuator_sockets.cpp
+++ rightbot_hardware_interface_pkgs/src/actuators/harmonic_motor_actuator/src/harmonic_motor_actuator_sockets.cpp
@@ -29,7 +29,7 @@ bool HarmonicMotorActuatorSockets::createSockets(int motor_id) {
 
     std::string can_interface;
     if(motor_name_ == "base_rotation_joint"){
-        can_interface = "can2";
+        can_interface = "can0";
     }
     else{
         can_interface = "can0";
