b04aa479a787b746b05579b3681c4e59586ff1cd
dheena-rb23 <dheenadhayalan.r@rightbot.in>
2024-03-06 13:41:13 +0530

    SHER-624 Correct velocity calculation on read funcion

--- rightbot_hardware_interface_pkgs/src/actuators/motor_actuator/src/motor_actuator.cpp
+++ rightbot_hardware_interface_pkgs/src/actuators/motor_actuator/src/motor_actuator.cpp
@@ -341,7 +341,7 @@ hardware_interface::return_type MotorActuator::read(const rclcpp::Time & time, c
         velocity_state_ = axis_* ((sensor_data["velocity"].asDouble()*travel_per_revolution)/(motor_gear_ratio*60));
     } else {
         // position_state_ = axis_*((((sensor_data["counts"].asInt() - initial_counts_rotation) + initial_counts_offset)*3.14*2)/(motor_ppr_*motor_gear_ratio));
-        velocity_state_ = axis_*((sensor_data["velocity"].asDouble()*3.14)/30);
+        velocity_state_ = axis_* ((sensor_data["velocity"].asDouble()*travel_per_revolution)/(motor_gear_ratio*60));
         position_state_ = axis_*((sensor_data["counts"].asInt() - initial_counts_rotation)*(travel_per_revolution)/(motor_ppr_*motor_gear_ratio));
         logger_->debug("[{}] Read pos debug: [{}], counts: [{}]", motor_name_, position_state_, sensor_data["counts"].asInt() );
     }
