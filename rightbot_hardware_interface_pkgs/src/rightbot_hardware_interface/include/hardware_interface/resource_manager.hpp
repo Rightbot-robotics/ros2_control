db62673cd319d5a206daa29bd73802480c2c97b8
dheena-rb23 <dheenadhayalan.r@rightbot.in>
2024-03-07 17:39:52 +0530

    SHER-624 Add conveyor belt actuator to low frequency components

--- rightbot_hardware_interface_pkgs/src/rightbot_hardware_interface/include/hardware_interface/resource_manager.hpp
+++ rightbot_hardware_interface_pkgs/src/rightbot_hardware_interface/include/hardware_interface/resource_manager.hpp
@@ -454,13 +454,15 @@ private:
     "conveyor_left_encoder",
     "conveyor_right_encoder",
     "conveyor_lift_left_actuator",
-    "conveyor_lift_right_actuator"
+    "conveyor_lift_right_actuator",
+    "conveyor_belt_actuator"
   };
   std::vector<std::string> node_guarding_components_{
     "right_armbase_actuator",
     "left_armbase_actuator",
     "conveyor_lift_left_actuator",
-    "conveyor_lift_right_actuator"
+    "conveyor_lift_right_actuator",
+    "conveyor_belt_actuator"
   };
   std::vector<std::string> sync_request_components_{
     "left_armbase_actuator",
